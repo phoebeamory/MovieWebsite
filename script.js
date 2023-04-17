e6f43e41c14b2b6d3b1799ed05f9ad43eb98055f
phoebeamory <121173399+phoebeamory@users.noreply.github.com>
2023-04-17 13:26:58 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let zachImg = document.querySelector(".zach");
+let asherImg = document.querySelector(".asher");
+let markImg = document.querySelector(".mark");
+let moviePoster = document.querySelector(".moviePoster");
+
+zachImg.onclick = function() {
+   window.location.href = "https://simple.wikipedia.org/wiki/Zachary_Levi";
+}
+
+asherImg.onclick = function() {
+   window.location.href = "https://simple.wikipedia.org/wiki/Asher_Angel";
+}
+
+markImg.onclick = function() {
+   window.location.href = "https://simple.wikipedia.org/wiki/Mark_Strong";
+}
+
+
+moviePoster.onclick = function() {
+   window.location.href = "https://www.cbr.com/shazam-dc-comics-trivia/";
+}
